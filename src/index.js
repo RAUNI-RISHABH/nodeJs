5daa73e8d488399ddb305d5fc6e3b65e3472e980
rishabh174 <rishabh.prakash42@gmail.com>
2024-12-03 14:24:58 +0530

    connect to mongodb Database

--- src/index.js
+++ src/index.js
@@ -1,11 +1,61 @@
-const express = require('express');
-require('dotenv').config();
-const app = express()
+// require('dotenv').config({path: './env'}) earlier approach with require/commonJS
+import dotenv from "dotenv";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+dotenv.config({
+  path: './env'
 })
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`)
-})
+
+import connectDB from "./db/connectionDb.js";
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Noraml appraoch with non modular approach
+// import mongoose from "mongoose";
+// import express from "express"
+// import { DB_NAME } from "./constants";
+// const app = express();
+
+// ( async () => {
+//   try {
+//    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+
+//    app.on("error", (err) => {
+
+//     console.log("ERR: ", err);
+//     throw err
+//    })
+
+//    app.listen(process.env.PORT, () => {
+//     console.log(`App is listening on port ${process.env.PORT}`);
+    
+//    });
+
+//   } catch (error) {
+//     console.error("ERROR: ", error);
+//     throw error
+//   }
+// })()
